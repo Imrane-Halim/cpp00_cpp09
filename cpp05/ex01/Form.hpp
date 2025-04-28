faa8345b8b168d6682468730b48e6b44fc06ccf4
Imrane Halim <ihalim.professional@gmail.com>
2025-04-28 10:56:45 +0100

    implemented form header file

---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef __FORM__
+#define __FORM__
+
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+class Form
+{
+	const std::string	_name;
+	bool				_signed;
+	const short			_signGrade;
+	const short			_execGrade;
+
+	class GradeTooHighException: public std::exception {
+	public:
+		const char*	what() const throw();
+	};
+	class GradeTooLowException: public std::exception {
+	public:
+		const char*	what() const throw();
+	};
+
+public:
+	Form(const std::string &name, short signGrade, short execGrade);
+	Form(const Form &copy);
+	~Form();
+
+	Form	&operator=(const Form &copy);
+
+	void	beSigned(const Bureaucrat &obj);
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &obj);
+
+#endif
