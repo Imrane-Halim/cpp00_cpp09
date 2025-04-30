a545f38b46fad9dc935d2ff6017fb4951dc1dcad
Imrane Halim <ihalim.professional@gmail.com>
2025-04-30 18:48:52 +0100

    implemented president class

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -1,37 +1,49 @@
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
-
-#include <unistd.h>
+#include "PresidentialPardonForm.hpp"
 
 int main(void) {
     // -----------><---------------
-    //try {
-    //    Bureaucrat  moron("jamal", 1);
-    //    ShrubberyCreationForm form("me", "pardon");
+    std::cout << "test 1:" << std::endl;
+    try {
+        Bureaucrat  moron("jamal", 1);
+        ShrubberyCreationForm form("me", "pardon");
 
-    //    moron.signForm(form);
-    //    form.execute(moron);
-    //}
-    //catch (const std::exception &e) {
-    //    std::cerr << "Error: " << e.what() << std::endl;
-    //} 
+        moron.signForm(form);
+        form.execute(moron);
+    }
+    catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    } 
 
     // -----------><---------------
-    //Bureaucrat  moron("jamal", 1);
-    //RobotomyRequestForm form("me", "robotonomy");
-    //for (int i = 0; i < 5; i++)
-    //{
-    //    try {
-    //        moron.signForm(form);
-    //        form.execute(moron);
-    //    }
-    //    catch (const std::exception &e) {
-    //        std::cerr << "Error: " << e.what() << std::endl;
-    //    }
-    //}
+    std::cout << "\n\ntest 2:" << std::endl;
+    Bureaucrat  moron("jamal", 1);
+    RobotomyRequestForm form("me", "robotonomy");
+    for (int i = 0; i < 5; i++)
+    {
+        try {
+            moron.signForm(form);
+            form.execute(moron);
+        }
+        catch (const std::exception &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
     
     // -----------><---------------
+    std::cout << "\n\ntest 2:" << std::endl;
+    try {
+        Bureaucrat  moron("jamal", 1);
+        PresidentialPardonForm form("me", "pardon");
+
+        moron.signForm(form);
+        form.execute(moron);
+    }
+    catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    } 
 
     return 0;
 }
