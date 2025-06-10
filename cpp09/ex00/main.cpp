4817c3eebc47b0c183475cae86982b6011018220
Imrane-Halim <ihalim.professional@gmail.com>
2025-06-10 22:20:30 +0100

    added csv file

---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		std::cerr << "usage: ./data <FILE>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	
+	return 0;
+}
