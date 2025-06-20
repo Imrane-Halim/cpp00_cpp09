a2c02055534577a7a55405365302b64e352fd66a
Imrane-Halim <ihalim.professional@gmail.com>
2025-06-20 15:33:04 +0100

    implemented ex00

---
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,71 @@
+#include "BitcoinExchange.hpp"
+
+// this operator was added so `std::tm` can be used as a key in std::map
+inline bool operator<(const std::tm& a, const std::tm& b)
+{
+    if (a.tm_year != b.tm_year)
+        return a.tm_year < b.tm_year;
+    if (a.tm_mon != b.tm_mon)
+        return a.tm_mon < b.tm_mon;
+    return a.tm_mday < b.tm_mday;
+}
+
+BitcoinExchange::BitcoinExchange()
+{
+    _load_db("data.csv");
+}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other): _records(other._records) { }
+BitcoinExchange::BitcoinExchange(const std::string& db_path)
+{
+   _load_db(db_path);
+}
+BitcoinExchange::~BitcoinExchange() { }
+
+BitcoinExchange&    BitcoinExchange::operator=(const BitcoinExchange& other)
+{
+    if (this != &other)
+        _records = other._records;
+    return *this;
+}
+
+void    BitcoinExchange::_load_db(const std::string& path)
+{
+    std::ifstream   db(path.c_str());
+    std::string     line;
+
+    if (!db.is_open())
+        throw(std::runtime_error("DB: " ERR_FILE + std::string(std::strerror(errno))));
+
+    // this line skips the first line in the file
+    std::getline(db, line);
+    // if (line != "date,exchange_rate")
+    //     throw(std::runtime_error("DB: " ERR_FIRSTLINE));
+
+    while (std::getline(db, line))
+    {
+        if (line.empty()) continue;
+
+        std::tm tm;
+        size_t  pos = line.find(',');
+        // these checks are unecessary sinse this is a DB but added anyway :)
+        // if (pos == std::string::npos)
+        //     throw(std::runtime_error("DB: " ERR_BADINPUT":" + line));
+        // if (!strptime(line.substr(0, pos).c_str(), "%Y-%m-%d", &tm))
+        //     throw(std::runtime_error("DB: " ERR_BADINPUT":" + line));
+        strptime(line.substr(0, pos).c_str(), "%Y-%m-%d", &tm);
+        _records[tm] = strtof(line.substr(pos + 1).c_str(), NULL);
+    }
+    db.close();
+}
+
+float   BitcoinExchange::getValue(std::tm& tm)
+{
+    dataBase::iterator it = _records.find(tm);
+    if (it != _records.end())
+        return it->second;
+    dataBase::reverse_iterator it1;
+    for (it1 = _records.rbegin(); it1 != _records.rend(); ++it1)
+        if (it1->first < tm) return it1->second;
+    // if we reached here it means date is too low
+    return -1;
+}
---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef __BITCOINEXCHANGE__
+#define __BITCOINEXCHANGE__
+
+#include <iostream>
+#include <map>
+#include <ctime>
+#include <sstream>
+#include <exception>
+#include <fstream>
+#include <cerrno>
+#include <cstring>
+#include <algorithm>
+
+#define ERR_FILE         "Can't open file: "
+#define ERR_BADINPUT     "Bad input!: "
+
+class BitcoinExchange
+{
+    typedef typename std::map<std::tm, float> dataBase;
+
+    dataBase _records;
+
+    // @note can throw exceptions
+    void            _load_db(const std::string& path);
+public:
+    // loads database from crnt root dict: ./data.csv
+    BitcoinExchange();
+    BitcoinExchange(const std::string& db_path);
+    BitcoinExchange(const BitcoinExchange& other);
+    ~BitcoinExchange();
+
+    BitcoinExchange&    operator=(const BitcoinExchange& other);
+
+    // @return btc value at `date`, if `date` 
+    //  is not in DB it returns lowest closest date
+    float           getValue(std::tm& date);
+};
+
+#endif
