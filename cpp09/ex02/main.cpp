a0950c16ab959b0feadd608bacd8afbb892f7a27
Imrane Halim <ihalim.professional@gmail.com>
2025-07-11 17:51:58 +0100

    added time measurment

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -19,14 +19,26 @@ int main(int ac, char **av)
         return EXIT_FAILURE;
     }
     
+    std::clock_t start, end;
+    double       vc_time, dq_time;
+
+    start = std::clock();
     std::vector<int> vc(arr, arr + ac - 1);
     sortVector(vc);
-
+    end = std::clock();
+    vc_time = (double)(end - start) / CLOCKS_PER_SEC;
+    
+    start = std::clock();
     std::deque<int> dq(arr, arr + ac - 1);
     sortDeque(dq);
-
+    end = std::clock();
+    dq_time = (double)(end - start) / CLOCKS_PER_SEC;
+    
     printNums(arr, ac - 1, "Before: ");
     printNums(vc, vc.size(), "After : ");
+    std::cout << std::fixed;
+    std::cout << "std::vecotr time: " << vc_time << "s\n";
+    std::cout << "std::deque time : " << dq_time << "s\n";
 
     delete[] arr;
 
